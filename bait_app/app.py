beeeb7222b8cda44ea166dfaedb4b7dc51542a64
skypaw <53307746+skypaw@users.noreply.github.com>
2022-10-08 17:28:55 +0200

    Add cookies and requestheaders to the logging

--- bait_app/app.py
+++ bait_app/app.py
@@ -1,10 +1,9 @@
-from flask import Flask, make_response, render_template, request, redirect, url_for, render_template_string
+from flask import Flask, make_response, render_template, render_template_string, request
 
-from bait_app import auth, log
+from bait_app import auth
 
 app = Flask(__name__)
 app.register_blueprint(auth.bp)
-app.register_blueprint(log.bp)
 
 
 @app.route("/", methods=['GET'])
@@ -17,6 +16,20 @@ def index():
 @app.route("/<path:path>", methods=['GET', 'POST', 'HEAD', 'PUT', 'DELETE'])
 def catch_all(path):
     return render_template_string('Found')
+
+
+@app.after_request
+def log(response):
+    request_data = {"User Agent": request.user_agent.__str__(),
+                    "Ip": request.remote_addr,
+                    "Path": request.full_path,
+                    "Method": request.method,
+                    "Form": request.form,
+                    "Cookies": request.cookies,
+                    "RequestHeaders": request.headers.__str__(),
+                    }
+    app.logger.warning(request_data)
+    return response
 
 
 if __name__ == '__main__':
